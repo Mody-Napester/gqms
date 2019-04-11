f22e00c2fedc90eb9fc092672f5112f4fad0e914
Ahmed Samy <ahmsam39@yahoo.com>
2019-04-11 15:57:20 +0200

    Adding Screens

---
+++ database/seeds/ScreenTypesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+//use DB;
+
+class ScreenTypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = [['en'=>'Kiosk', 'ar'=>'كشك'],['en'=>'Reception', 'ar'=>'ريسبشن']];
+
+        foreach ($types as $type){
+            \Illuminate\Support\Facades\DB::table('screen_types')->insert([
+                'name_ar' => $type['ar'],
+                'name_en' => $type['en']
+            ]);
+        }
+    }
+}
